36475dd74f0378f06415c0cf6b36e043b8c96181
oiosu <kdlatnrud123@naver.com>
2023-01-30 15:15:44 +0900

    조건문 실습문제(1)

---
+++ PYTHON_BASIC/myvenv/Chapter05/03.실습문제.5.1.2.py
@@ -0,0 +1,11 @@
+# 실습문제 5.1.2
+
+study_time = int(input("공부시간을 입력하세요(시간) >>>"))
+# print(study_time) 테스트하기 
+
+if study_time >= 10:
+    print("휴대폰 잠금이 해제됩니다.")
+elif study_time >= 5:
+    print("휴대폰을 30분간 사용가능 합니다.")
+else:
+    print("휴대폰 사용이 불가능합니다.")
