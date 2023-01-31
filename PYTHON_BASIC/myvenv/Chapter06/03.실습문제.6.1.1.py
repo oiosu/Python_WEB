a00e66a1aeb7058d326fe3ebb19d4fb1b730210e
oiosu <kdlatnrud123@naver.com>
2023-02-01 00:43:00 +0900

    함수실습문제(1)

---
+++ PYTHON_BASIC/myvenv/Chapter06/03.실습문제.6.1.1.py
@@ -0,0 +1,19 @@
+# 실습문제 6.1.1 
+
+# 다음은 두수의 곱셈을 반환하는 multiply함수이다. 
+# multiply 함수를 호출하는 방법으로 옳은 것은? 
+
+# docstring : 설명문
+
+def multiply(x, y):
+    """
+    두수의 곱셈을 반환하는 함수 
+    매개변수 x : 숫자
+    매개변수 y : 숫자 
+    """
+    result = x * y
+    return result 
+multiply(3, 4) #정답 3번 
+
+
+
